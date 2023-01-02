b3027936ff323df63da70b4a14e34cec6fb14840
mgambrell <mgambrell@users.noreply.github.com>
2023-01-02 15:58:04 -0500

    etc

--- src/rtlib/sys_chdir.c
+++ src/rtlib/sys_chdir.c
@@ -2,12 +2,10 @@
 
 #include "fb.h"
 
-/* blackbox systems don't have chdir, so private implementation is needed */
-/* TODO: make blackbox API instead so common string handling etc. are handled here */
-#ifndef HOST_FB_BLACKBOX
-
 #ifdef HOST_MINGW
 #include <direct.h>
+#elif defined(HOST_FB_BLACKBOX)
+int HOST_FB_BLACKBOX_fb_ChDir(const char* path);
 #else
 #include <unistd.h>
 #endif
@@ -18,6 +16,8 @@ FBCALL int fb_ChDir( FBSTRING *path )
 
 #ifdef HOST_MINGW
 	res = _chdir( path->data );
+	#elif defined(HOST_FB_BLACKBOX)
+	res = HOST_FB_BLACKBOX_fb_ChDir(path->data);
 #else
 	res = chdir( path->data );
 #endif
@@ -27,5 +27,3 @@ FBCALL int fb_ChDir( FBSTRING *path )
 
 	return res;
 }
-
-#endif
